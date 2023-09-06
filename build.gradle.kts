b2774a33d83b2a4a5ab75e814ac3c7e99e45aca8
Vivek Kumar <vicky.chadhary67@gmail.com>
2023-09-07 01:01:08 +0530

    dependency_centralisation/1.0/gradle_dependency_version_toml - All deps will we kept in a config file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    id("com.android.library").version("7.4.2").apply(false)
-    kotlin("multiplatform").version("1.8.21").apply(false)
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.multiplatform) apply false
 }
 
 tasks.register("clean", Delete::class) {
